d3f90da3a7b3dc17c572fcc501261a98bdf3825d
DenysPolitiuk <denys.politiuk@protonmail.com>
2020-07-01 16:19:45 -0400

    Adding option to output in different memory units Also some code refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,28 @@ use heim::{process, process::Process, units::information};
 use std::collections::HashMap;
 use std::error::Error;
 
+enum MemoryUnit {
+    B,
+    KB,
+    MB,
+    GB,
+}
+
+impl MemoryUnit {
+    fn get_type(&self) -> &str {
+        match self {
+            MemoryUnit::B => "B",
+            MemoryUnit::KB => "KB",
+            MemoryUnit::MB => "MB",
+            MemoryUnit::GB => "GB",
+        }
+    }
+
+    fn get_value_string(&self, value: u64) -> String {
+        format!("{} {}", value, self.get_type())
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -27,69 +49,96 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .long("details")
                 .help("print more information about the process"),
         )
+        .arg(
+            Arg::with_name("byte")
+                .conflicts_with_all(&["kilobyte", "gigabyte"])
+                .short("b")
+                .long("byte")
+                .help("use bytes to display memory usage"),
+        )
+        .arg(
+            Arg::with_name("kilobyte")
+                .conflicts_with_all(&["byte", "gigabyte"])
+                .short("k")
+                .long("kilobyte")
+                .help("use kilobytes to display memory usage"),
+        )
+        .arg(
+            Arg::with_name("gigabyte")
+                .conflicts_with_all(&["kilobyte", "byte"])
+                .short("g")
+                .long("gigabyte")
+                .help("use gigabytes to display memory usage"),
+        )
         .get_matches();
     let target = matches.value_of("pid").unwrap();
     let target_pid = target.parse::<u32>();
+
+    let unit_to_use = if matches.is_present("byte") {
+        MemoryUnit::B
+    } else if matches.is_present("kilobyte") {
+        MemoryUnit::KB
+    } else if matches.is_present("gigabyte") {
+        MemoryUnit::GB
+    } else {
+        MemoryUnit::MB
+    };
 
     let include_children = matches.is_present("children");
     let is_details = matches.is_present("details");
 
     let rss = match target_pid {
         Ok(target_pid) => {
-            if include_children {
-                process_ram_usage_full(target_pid as i32, is_details).await?
-            } else {
-                process_ram_usage_single(target_pid as i32, is_details).await?
-            }
+            process_ram_usage(target_pid as i32, is_details, include_children, unit_to_use).await?
         }
-        Err(_) => process_ram_usage_named(target, is_details, include_children).await?,
+        Err(_) => {
+            process_ram_usage_named(target, is_details, include_children, unit_to_use).await?
+        }
     };
 
-    println!("{} MB", rss);
+    println!("{}", rss);
 
     Ok(())
 }
 
-async fn process_ram_usage_single(pid: i32, print_details: bool) -> Result<u64, Box<dyn Error>> {
+async fn process_ram_usage(
+    pid: i32,
+    print_details: bool,
+    include_children: bool,
+    unit: MemoryUnit,
+) -> Result<String, Box<dyn Error>> {
     let process = process::get(pid).await?;
-    let rss = process.memory().await?.rss().get::<information::megabyte>();
+    let mut rss = get_memory_value(&process, &unit).await?;
 
     if print_details {
-        print_process_info(&process).await?;
-        println!("");
+        print_process_info(&process, &unit).await?;
     }
 
-    Ok(rss)
-}
+    if !include_children {
+        return Ok(unit.get_value_string(rss));
+    }
 
-async fn process_ram_usage_full(pid: i32, print_details: bool) -> Result<u64, Box<dyn Error>> {
     let processes = get_all_processes().await?;
-    let process = process::get(pid).await?;
-    let mut rss = process.memory().await?.rss().get::<information::megabyte>();
-
-    if print_details {
-        print_process_info(&process).await?;
-    }
-
     let all_children = get_all_children(&processes, pid);
     if let Some(children) = all_children {
         for child in &children {
-            rss += child.memory().await?.rss().get::<information::megabyte>();
+            rss += get_memory_value(&child, &unit).await?;
             if print_details {
-                print_process_info(&child).await?;
+                print_process_info(&child, &unit).await?;
                 println!("");
             }
         }
     }
 
-    Ok(rss)
+    Ok(format!("{} {}", rss, unit.get_type()))
 }
 
 async fn process_ram_usage_named(
     name: &str,
     print_details: bool,
     include_children: bool,
-) -> Result<u64, Box<dyn Error>> {
+    unit: MemoryUnit,
+) -> Result<String, Box<dyn Error>> {
     let processes = get_all_processes().await?;
     let self_process = process::current().await?.pid();
 
@@ -134,14 +183,14 @@ async fn process_ram_usage_named(
 
     let mut rss = 0;
     for process in final_results.values() {
-        rss += process.memory().await?.rss().get::<information::megabyte>();
+        rss += get_memory_value(&process, &unit).await?;
         if print_details {
-            print_process_info(&process).await?;
+            print_process_info(&process, &unit).await?;
             println!("");
         }
     }
 
-    Ok(rss)
+    Ok(format!("{} {}", rss, unit.get_type()))
 }
 
 fn get_all_children(processes: &Vec<(Process, i32)>, parent_pid: i32) -> Option<Vec<&Process>> {
@@ -190,18 +239,33 @@ fn get_children(processes: &Vec<(Process, i32)>, parent_pid: i32) -> Option<Vec<
     }
 }
 
-async fn print_process_info(process: &Process) -> Result<(), Box<dyn Error>> {
+async fn print_process_info(
+    process: &Process,
+    unit_type: &MemoryUnit,
+) -> Result<(), Box<dyn Error>> {
     println!("{:>10} {:?}", "PID", process.pid());
     println!("{:>10} {:?}", "Parent pid", process.parent_pid().await?);
     println!("{:>10} {:?}", "Name", process.name().await?);
     println!("{:>10} {:?}", "Exe", process.exe().await?);
     println!("{:>10} {:?}", "Command", process.command().await?);
     println!(
-        "{:>10} {:?} MB",
+        "{:>10} {}",
         "RSS",
-        process.memory().await?.rss().get::<information::megabyte>()
+        unit_type.get_value_string(get_memory_value(&process, &unit_type).await?),
     );
     println!("{:>10} {:?}", "Status", process.status().await?);
 
     Ok(())
+}
+
+async fn get_memory_value(
+    process: &Process,
+    unit_type: &MemoryUnit,
+) -> Result<u64, Box<dyn Error>> {
+    match unit_type {
+        MemoryUnit::B => Ok(process.memory().await?.rss().get::<information::byte>()),
+        MemoryUnit::KB => Ok(process.memory().await?.rss().get::<information::kilobyte>()),
+        MemoryUnit::MB => Ok(process.memory().await?.rss().get::<information::megabyte>()),
+        MemoryUnit::GB => Ok(process.memory().await?.rss().get::<information::gigabyte>()),
+    }
 }
