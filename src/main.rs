3f613fdce5f2175627fe11e158a6355fb1966c86
DenysPolitiuk <denys.politiuk@protonmail.com>
2020-07-01 20:10:21 -0400

    Work in progress on process watching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use clap::{App, Arg};
+use ctrlc;
 use futures::StreamExt;
 use heim::{process, process::Process, units::information};
+use tokio::time::delay_for;
 
 use std::collections::HashMap;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 enum MemoryUnit {
     B,
@@ -24,6 +28,35 @@ impl MemoryUnit {
 
     fn get_value_string(&self, value: u64) -> String {
         format!("{} {}", value, self.get_type())
+    }
+}
+
+enum WatchAction {
+    Kill,
+    Print,
+}
+
+impl WatchAction {
+    fn is_valid_action(action_text: &str) -> bool {
+        if action_text == "kill" || action_text == "print" {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn new(action_text: &str) -> Option<WatchAction> {
+        if !WatchAction::is_valid_action(action_text) {
+            None
+        } else {
+            if action_text == "kill" {
+                Some(WatchAction::Kill)
+            } else if action_text == "print" {
+                Some(WatchAction::Print)
+            } else {
+                None
+            }
+        }
     }
 }
 
@@ -70,6 +103,39 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .long("gigabyte")
                 .help("use gigabytes to display memory usage"),
         )
+        .arg(
+            Arg::with_name("watch")
+                .conflicts_with("details")
+                .requires("limit")
+                .short("w")
+                .long("watch")
+                .help("watch for process memory usage"),
+        )
+        .arg(
+            Arg::with_name("limit")
+                .requires("watch")
+                .short("l")
+                .long("limit")
+                .takes_value(true)
+                .help("limit of memory process can have before process is actioned on")
+        )
+        .arg(
+            Arg::with_name("interval")
+            .requires("watch")
+            .short("i")
+            .long("interval")
+            .takes_value(true)
+            .help("interval to check memory on a process in seconds, default 1 sec")
+        )
+        .arg(
+            Arg::with_name("action")
+            .requires_all(&["watch", "limit"])
+            .short("a")
+            .long("action")
+            .takes_value(true)
+            .help("action to take when the limit is hit, default print\npossible values:\n\tprint - print process info\n\tkill - kill process")
+            .validator(is_valid_action)
+        )
         .get_matches();
     let target = matches.value_of("pid").unwrap();
     let target_pid = target.parse::<u32>();
@@ -86,20 +152,154 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let include_children = matches.is_present("children");
     let is_details = matches.is_present("details");
+    let is_watch = matches.is_present("watch");
+    let limit_value = matches.value_of("limit").map(|v| v.parse::<u64>().ok());
+    let interval_value = matches
+        .value_of("interval")
+        .or(Some("1"))
+        .unwrap()
+        .parse::<u64>()
+        .expect("invalid interval value");
+    let action_type = match matches.value_of("action") {
+        None => Some(WatchAction::Print),
+        Some(action) => WatchAction::new(action),
+    }
+    .expect("invalid action");
 
-    println!(
-        "{}",
+    if is_watch {
+        let limit_value = limit_value
+            .expect("missing limit value")
+            .expect("invalid limit value");
+        // TODO: add optional children inclusion in processes ram usage
         match target_pid {
             Ok(target_pid) => {
-                process_ram_usage(target_pid as i32, is_details, include_children, unit_to_use)
-                    .await?
+                watch_process(
+                    target_pid as i32,
+                    interval_value,
+                    action_type,
+                    limit_value,
+                    &unit_to_use,
+                )
+                .await?
             }
             Err(_) => {
-                process_ram_usage_named(target, is_details, include_children, unit_to_use).await?
+                watch_process_named(
+                    target,
+                    interval_value,
+                    action_type,
+                    limit_value,
+                    &unit_to_use,
+                )
+                .await?
             }
-        }
-    );
+        };
+    } else {
+        println!(
+            "{}",
+            match target_pid {
+                Ok(target_pid) => {
+                    process_ram_usage(
+                        target_pid as i32,
+                        is_details,
+                        include_children,
+                        &unit_to_use,
+                    )
+                    .await?
+                    .0
+                }
+                Err(_) => {
+                    process_ram_usage_named(target, is_details, include_children, unit_to_use)
+                        .await?
+                }
+            }
+        );
+    }
 
+    Ok(())
+}
+
+fn is_valid_action(val: String) -> Result<(), String> {
+    if !WatchAction::is_valid_action(&val) {
+        Err(String::from("invalid action"))
+    } else {
+        Ok(())
+    }
+}
+
+async fn watch_process(
+    pid: i32,
+    interval: u64,
+    action: WatchAction,
+    limit: u64,
+    unit_type: &MemoryUnit,
+) -> Result<(), Box<dyn Error>> {
+    let exit = Arc::new(Mutex::new(false));
+    let e = exit.clone();
+    let _ = ctrlc::set_handler(move || {
+        let mut exit = e.lock().unwrap();
+        *exit = true;
+    });
+
+    while !*exit.lock().unwrap() {
+        let rss = process_ram_usage(pid, false, false, unit_type).await?;
+        if rss.1 >= limit {
+            match action {
+                WatchAction::Print => println!(
+                    "process {} reached limit {}{}, currently at {}",
+                    pid,
+                    limit,
+                    unit_type.get_type(),
+                    rss.0
+                ),
+                WatchAction::Kill => {
+                    terminate_process(pid).await?;
+                    break;
+                }
+            };
+        }
+        delay_for(Duration::from_secs(interval)).await;
+    }
+
+    Ok(())
+}
+
+async fn watch_process_named(
+    name: &str,
+    interval: u64,
+    action: WatchAction,
+    limit: u64,
+    unit_type: &MemoryUnit,
+) -> Result<(), Box<dyn Error>> {
+    let processes = get_all_processes().await?;
+    let possible_processes = get_process_from_name(name, &processes).await?;
+
+    if possible_processes.len() > 1 {
+        println!("more than 1 possible process with given name");
+        println!(
+            "found {} processes matching name {}",
+            possible_processes.len(),
+            name
+        );
+        return Ok(());
+    } else if possible_processes.is_empty() {
+        println!("did not find process with name {}", name);
+        return Ok(());
+    }
+
+    let mut pid = -1;
+    for (a_pid, _) in possible_processes {
+        pid = a_pid;
+        // should only be one, should be handled with checks before
+        break;
+    }
+
+    watch_process(pid, interval, action, limit, unit_type).await?;
+
+    Ok(())
+}
+
+async fn terminate_process(pid: i32) -> Result<(), Box<dyn Error>> {
+    process::get(pid).await?.terminate().await?;
     Ok(())
 }
 
@@ -107,8 +307,8 @@ async fn process_ram_usage(
     pid: i32,
     print_details: bool,
     include_children: bool,
-    unit: MemoryUnit,
-) -> Result<String, Box<dyn Error>> {
+    unit: &MemoryUnit,
+) -> Result<(String, u64), Box<dyn Error>> {
     let process = process::get(pid).await?;
     let mut rss = get_memory_value(&process, &unit).await?;
 
@@ -117,7 +317,7 @@ async fn process_ram_usage(
     }
 
     if !include_children {
-        return Ok(unit.get_value_string(rss));
+        return Ok((unit.get_value_string(rss), rss));
     }
 
     let processes = get_all_processes().await?;
@@ -132,23 +332,18 @@ async fn process_ram_usage(
         }
     }
 
-    Ok(format!("{} {}", rss, unit.get_type()))
+    Ok((format!("{} {}", rss, unit.get_type()), rss))
 }
 
-async fn process_ram_usage_named(
+async fn get_process_from_name<'a>(
     name: &str,
-    print_details: bool,
-    include_children: bool,
-    unit: MemoryUnit,
-) -> Result<String, Box<dyn Error>> {
-    let processes = get_all_processes().await?;
+    processes: &'a Vec<(Process, i32)>,
+) -> Result<HashMap<i32, &'a Process>, Box<dyn Error>> {
     let self_process = process::current().await?.pid();
 
-    // need to use hashmaps because named search can find both a process and its children
-    // which creates "double" entries
     let mut results = HashMap::new();
 
-    for (process, _) in &processes {
+    for (process, _) in processes {
         let process_name = process.name().await?;
         let process_command = process
             .command()
@@ -165,6 +360,17 @@ async fn process_ram_usage_named(
         }
     }
 
+    Ok(results)
+}
+
+async fn process_ram_usage_named(
+    name: &str,
+    print_details: bool,
+    include_children: bool,
+    unit: MemoryUnit,
+) -> Result<String, Box<dyn Error>> {
+    let processes = get_all_processes().await?;
+    let results = get_process_from_name(name, &processes).await?;
     let mut final_results: HashMap<i32, &Process> = HashMap::new();
 
     if include_children {
